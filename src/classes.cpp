479ac498405a51aa67210db73ac47913937026d4
JohnnyCena123 <iamahuman1395@gmail.com>
2025-03-06 19:42:07 +0200

    initial commint

---
+++ src/classes.cpp
@@ -0,0 +1,319 @@
+#include <Geode/Geode.hpp>
+
+#include "alphalaneous.pages_api/include/PageMenu.h"
+
+#include "classes.hpp"
+
+using namespace geode::prelude;
+
+
+
+
+bool isSwitcherOn = true;
+
+#include <Geode/modify/PlayLayer.hpp>
+class $modify(MyPlayLayer, PlayLayer) {
+
+    static void onModify(auto& self) {
+        log::info("Hooking PlayLayer...");
+        if (!self.setHookPriorityPre("PlayLayer::resume", Priority::Last)) {
+            log::warn("Failed to set hook priority for PlayLayer::resume.");
+        }
+        if (!self.setHookPriorityPre("PlayLayer::resumeAndRestart", Priority::Last)) {
+            log::warn("Failed to set hook priority for PlayLayer::resumeAndRestart.");
+        }
+        if (!self.setHookPriorityPre("PlayLayer::resetLevel", Priority::Last)) {
+            log::warn("Failed to set hook priority for PlayLayer::resetLevel.");
+        }
+    }
+
+    struct Fields {
+        bool m_isPracticeMode = false;
+
+        CheckpointObject* m_selectedCheckpoint = nullptr;
+    };
+
+    bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
+        if (!PlayLayer::init(level, useReplay, dontCreateObjects)) return false;
+        
+        return true;
+    } 
+
+    void setCheckpoint(CheckpointObject* checkpoint) {
+        m_fields->m_selectedCheckpoint = checkpoint;
+    }
+
+    // void resume() {
+    //     if (m_fields->m_selectedCheckpoint != nullptr && isSwitcherOn) {
+    //         PlayLayer::loadFromCheckpoint(m_fields->m_selectedCheckpoint);
+    //     } else {
+    //         resume();
+    //     }
+    // }
+
+    // void resumeAndRestart(bool p0) {
+    //     if (m_fields->m_selectedCheckpoint != nullptr && isSwitcherOn) {
+    //         PlayLayer::loadFromCheckpoint(m_fields->m_selectedCheckpoint);
+    //     } else {
+    //         resumeAndRestart(p0);
+    //     }
+    // }
+
+    void resetLevel() {
+        if (m_fields->m_selectedCheckpoint != nullptr && isSwitcherOn) {
+            PlayLayer::loadFromCheckpoint(m_fields->m_selectedCheckpoint);
+        } else {
+            resetLevel();
+        }
+    }
+
+    bool getIsPracticeMode() {
+        return m_fields->m_isPracticeMode;
+    }
+
+    CCArray* getCheckpoints() {
+        return m_checkpointArray;
+    }
+
+    void togglePracticeMode(bool practiceMode) {
+        
+        PlayLayer::togglePracticeMode(practiceMode);
+
+        m_fields->m_isPracticeMode = practiceMode;
+    }
+        
+};
+
+// #include <Geode/modify/CheckpointObject.hpp>
+// class $modify(MyCheckpointObject, CheckpointObject) {
+
+//     struct Fields {
+        
+//     };
+
+//     bool init() {
+//         if (!CheckpointObject::init()) return false;
+        
+//         if (Mod::get()->getSettingValue<bool>("EnablePreviews")) {
+
+//         }
+
+//         return true;
+//     }
+
+// };
+
+bool CheckpointSwitcherLayer::setup() {
+
+    s_currentLayer = this;
+
+    this->setID("CheckpointSwitcherLayer"_spr);
+
+    this->setTitle("Choose checkpoint");
+
+    m_selectedButton = nullptr;
+    
+    m_toggleSwitcherButtonSprite = CCSprite::createWithSpriteFrameName("GJ_checkOff_001.png");
+    m_toggleSwitcherButtonCheckmarkSprite = CCSprite::createWithSpriteFrameName("GJ_completesIcon_001.png");
+    m_toggleSwitcherButtonCheckmarkSprite->setVisible(isSwitcherOn);
+    m_toggleSwitcherButtonSprite->addChildAtPosition(m_toggleSwitcherButtonCheckmarkSprite, Anchor::Center);
+    m_toggleSwitcherButton = CCMenuItemSpriteExtra::create(m_toggleSwitcherButtonSprite, this, menu_selector(CheckpointSwitcherLayer::onToggleSwitcher));
+    m_toggleSwitcherButton->ignoreAnchorPointForPosition(true);
+    m_buttonMenu->addChildAtPosition(m_toggleSwitcherButton, Anchor::BottomLeft, ccp(10.f, 10.f));
+    
+    m_toggleSwitcherButtonLabel = CCLabelBMFont::create("Enable the switcher!", "bigFont.fnt");
+    m_toggleSwitcherButtonLabel->setScale(0.333f);
+    m_toggleSwitcherButtonLabel->ignoreAnchorPointForPosition(true);
+    m_mainLayer->addChildAtPosition(m_toggleSwitcherButtonLabel, Anchor::BottomRight, ccp(20.f + m_toggleSwitcherButton->getContentWidth(), 10.f));
+
+    m_applyButtonEnabledSprite = ButtonSprite::create("Apply");
+    m_applyButtonDisabledSprite = ButtonSprite::create("Apply");
+    m_applyButtonDisabledSprite->setOpacity(155);
+    m_applyButtonDisabledSprite->setColor(ccGRAY);
+    m_applyButton = CCMenuItemSpriteExtra::create(m_applyButtonEnabledSprite, m_applyButtonDisabledSprite, this, menu_selector(CheckpointSwitcherLayer::onApply));
+    m_applyButton->setEnabled(false);
+    m_buttonMenu->addChildAtPosition(m_applyButton, Anchor::Bottom, ccp(0, m_applyButton->getContentHeight() / 2.f + 10.f));
+
+    m_checkpointSelectorMenu = CCMenu::create();
+    m_checkpointSelectorMenu->setLayout(
+        RowLayout::create()
+            ->setGap(30.f)
+            ->setGrowCrossAxis(false)
+            ->setAxisAlignment(AxisAlignment::Even)
+            ->setCrossAxisOverflow(false)
+    );
+    m_mainLayer->addChildAtPosition(m_checkpointSelectorMenu, Anchor::Center);
+    m_checkpointSelectorMenu->setContentWidth(m_mainLayer->getContentWidth());
+
+    auto pagedCheckpointSelectorMenu = static_cast<PageMenu*>(m_checkpointSelectorMenu);
+    pagedCheckpointSelectorMenu->setPaged(3, HORIZONTAL, 600.f, 4);
+
+    m_currentPlayLayer = static_cast<MyPlayLayer*>(CCScene::get()->getChildByID("PlayLayer"));
+    m_checkpoints = m_currentPlayLayer->getCheckpoints();
+    m_isPracticeMode = m_currentPlayLayer->getIsPracticeMode();
+
+    
+    m_buttonsArray = CCArray::create();
+    
+
+    if (!m_isPracticeMode) {
+        auto labelPracticeOff = CCNode::create();
+        auto labelPracticeOff1 = CCLabelBMFont::create("Enable practice mode", "bigFont.fnt");
+        auto labelPracticeOff2 = CCLabelBMFont::create("to use the mod!", "bigFont.fnt");
+
+        labelPracticeOff1->setPositionY(20);
+        labelPracticeOff2->setPositionY(-20);
+
+        labelPracticeOff->setPosition(m_mainLayer->getContentWidth() / 2.f, m_mainLayer->getContentHeight() / 2.f);
+
+        m_mainLayer->addChild(labelPracticeOff);
+        labelPracticeOff->addChild(labelPracticeOff1);
+        labelPracticeOff->addChild(labelPracticeOff2);
+
+        labelPracticeOff->setID("label-practice-off");
+
+
+        m_applyButton->removeFromParent();
+
+    } else {
+        
+        for (int i = 0; i < m_checkpoints->count(); i++) {
+
+            auto checkpoint = static_cast<CheckpointObject*>(m_checkpoints->objectAtIndex(i));
+
+            auto checkpointButton = CheckpointSelectorButton::create(i, checkpoint);
+            m_checkpointSelectorMenu->addChild(checkpointButton);
+            m_buttonsArray->addObject(checkpointButton);
+            checkpointButton->setID(fmt::format("checkpoint-button-no-{}", i).c_str());
+
+        }
+
+        m_checkpointSelectorMenu->updateLayout();
+
+    }
+    
+    m_toggleSwitcherButtonSprite->setID("toggle-switcher-checkbox");
+    m_toggleSwitcherButtonCheckmarkSprite->setID("toggle-switcher-checkbox-checkmark");
+    m_toggleSwitcherButton->setID("toggle-checkpoint-switcher-button");
+    m_applyButtonEnabledSprite->setID("apply-button-enabled-sprite");
+    m_applyButtonDisabledSprite->setID("apply-button-enabled-sprite");
+    m_applyButton->setID("apply-button");
+    m_checkpointSelectorMenu->setID("checkpoint-selector-menu");
+
+    m_buttonMenu->setID("button-menu");
+    m_mainLayer->setID("main-layer");
+    m_title->setID("title");
+    m_closeBtn->setID("close-button");
+    m_bgSprite->setID("background-sprite");
+
+    return true;
+}
+
+void CheckpointSwitcherLayer::onToggleSwitcher(CCObject* sender) {
+    isSwitcherOn = !isSwitcherOn;
+    m_toggleSwitcherButtonCheckmarkSprite->setVisible(isSwitcherOn);
+}
+
+void CheckpointSwitcherLayer::onApply(CCObject* sender) {
+    m_currentPlayLayer->setCheckpoint(m_selectedCheckpoint);
+    m_applyButton->setEnabled(false);
+}
+
+void CheckpointSwitcherLayer::selectCheckpoint(CheckpointObject* checkpoint) {
+    m_selectedCheckpoint = checkpoint;
+}
+
+void CheckpointSwitcherLayer::toggleApplyButton(bool isEnabled) {
+    m_applyButton->setEnabled(isEnabled);
+}
+
+CheckpointSwitcherLayer* CheckpointSwitcherLayer::create() {
+    auto ret = new CheckpointSwitcherLayer();
+    if (ret->initAnchored(480.f, 240.f)) {
+        ret->autorelease();
+        return ret;
+    }
+
+    delete ret;
+    return nullptr;
+}
+
+CheckpointSwitcherLayer* CheckpointSwitcherLayer::get() { 
+    return s_currentLayer;
+}
+
+CheckpointSwitcherLayer::~CheckpointSwitcherLayer() {
+    s_currentLayer = nullptr;
+}
+
+CheckpointSwitcherLayer* CheckpointSwitcherLayer::s_currentLayer = nullptr;
+
+    
+    
+bool CheckpointSelectorButton::init(int buttonID, CheckpointObject* checkpoint) {
+    m_checkpointSprite = CCSprite::createWithSpriteFrameName("checkpoint_01_001.png");
+    if (!CCMenuItemSpriteExtra::init(m_checkpointSprite, m_checkpointSprite, this, menu_selector(CheckpointSelectorButton::onSelectButton))) return false;
+
+    m_checkpoint = checkpoint;
+
+    m_checkpointSprite->setScale(80 / m_checkpointSprite->getContentHeight());
+    this->setScale(80 / this->getContentHeight());
+
+    m_checkpointOutline = CCSprite::createWithSpriteFrameName("checkpoint_01_color_001.png");
+    if (!m_checkpointOutline) {log::error("checkpoint outline failed to initialize."); return false;}
+    m_checkpointOutline->setVisible(false);
+    m_checkpointOutline->setColor(ccc3(255, 243, 69));
+    m_checkpointSprite->addChildAtPosition(m_checkpointOutline, Anchor::Center);
+
+    m_checkpointGlowOutline = CCSprite::createWithSpriteFrameName("checkpoint_01_glow_001.png");
+    if (!m_checkpointGlowOutline) {log::error("checkpoint glow outline failed to initialize."); return false;}
+    m_checkpointGlowOutline->setColor(ccc3(255, 253, 137));
+    m_checkpointOutline->addChildAtPosition(m_checkpointGlowOutline, Anchor::Center);
+
+    m_buttonLabel = CCLabelBMFont::create(fmt::format("Checkpoint #{}", buttonID + 1).c_str(), "bigFont.fnt");
+    if (!m_buttonLabel) {log::error("button label failed to initialize."); return false;}
+    m_checkpointSprite->addChild(m_buttonLabel);
+    m_buttonLabel->setPosition(m_checkpointSprite->getContentWidth() / 2.f, m_checkpointSprite->getContentHeight() + 5.f);
+    m_buttonLabel->setScale(0.2);
+
+    m_checkpointSprite->setID("checkpoint-sprite");
+    m_buttonLabel->setID("checkpoint-label");
+    m_checkpointOutline->setID("checkpoint-outline");
+    m_checkpointGlowOutline->setID("checkpoint-glow-outline");
+
+    return true;
+    
+}
+
+void CheckpointSelectorButton::onSelectButton(CCObject* sender) {
+
+    auto currentPlayLayer = static_cast<MyPlayLayer*>(CCScene::get()->getChildByID("PlayLayer"));
+
+    auto lastSelectedButton = CheckpointSwitcherLayer::get()->m_selectedButton;
+
+    CheckpointSwitcherLayer::get()->selectCheckpoint(m_checkpoint);
+    CheckpointSwitcherLayer::get()->toggleApplyButton(true);
+
+    if (lastSelectedButton != nullptr) lastSelectedButton->setOutlineVisible(false);
+    this->setOutlineVisible(true);
+
+    if (lastSelectedButton != nullptr) lastSelectedButton->runAction(CCEaseInOut::create(CCScaleBy::create(0.1f, 0.8f), 2.f));
+    this->runAction(CCEaseInOut::create(CCScaleBy::create(0.1f, 1.25f), 2.f));
+
+    CheckpointSwitcherLayer::get()->m_selectedButton = this;
+}
+
+void CheckpointSelectorButton::setOutlineVisible(bool isVisible) {
+    m_checkpointOutline->setVisible(isVisible);
+}
+
+CheckpointSelectorButton* CheckpointSelectorButton::create(int buttonID, CheckpointObject* checkpoint) {
+    auto ret = new CheckpointSelectorButton();
+    if (ret->init(buttonID, checkpoint)) {
+        ret->autorelease();
+        return ret;
+    }
+
+    delete ret;
+    return nullptr;
+}
