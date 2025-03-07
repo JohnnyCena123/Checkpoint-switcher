78410cc712a2b79c5f5689e90d2600bd51612915
JohnnyCena123 <eitanmiller7@gmail.com>
2025-03-07 08:02:14 +0200

    .

--- src/classes.cpp
+++ src/classes.cpp
@@ -108,6 +108,7 @@ bool CheckpointSwitcherLayer::setup() {
     m_applyButtonEnabledSprite = ButtonSprite::create("Apply");
     m_applyButtonDisabledSprite = ButtonSprite::create("Apply");
     m_applyButtonDisabledSprite->setOpacity(155);
+    m_applyButtonDisabledSprite->setColor(isEnabled ? ccWHITE : ccGRAY);
     m_applyButton = CCMenuItemSpriteExtra::create(m_applyButtonEnabledSprite, m_applyButtonDisabledSprite, this, menu_selector(CheckpointSwitcherLayer::onApply));
     m_applyButton->setEnabled(false);
     m_buttonMenu->addChildAtPosition(m_applyButton, Anchor::Bottom, ccp(0, m_applyButton->getContentHeight() / 2.f + 10.f));
@@ -204,8 +205,6 @@ void CheckpointSwitcherLayer::selectCheckpoint(CheckpointObject* checkpoint) {
 
 void CheckpointSwitcherLayer::toggleApplyButton(bool isEnabled) {
     m_applyButton->setEnabled(isEnabled);
-	m_applyButtonEnabledSprite->setColor(isEnabled ? ccWHITE : ccGRAY);
-	m_applyButtonEnabledSprite->setOpacity(isEnabled ? 255 : 155);
 }
 
 CheckpointSwitcherLayer* CheckpointSwitcherLayer::create() {
